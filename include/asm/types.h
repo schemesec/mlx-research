30949169b89e153377112deaacd6f79a2c39a5b1
scheme <scheme@scheme.home.schemesec.com>
2026-05-16 12:26:36 -0400

    vendor generated x86 asm headers

--- include/asm/types.h
+++ include/asm/types.h
@@ -1,4 +1,1 @@
-#ifndef _OFED_LOCAL_ASM_TYPES_H
-#define _OFED_LOCAL_ASM_TYPES_H
-#include "/usr/src/linux-headers-7.0.2-2-pve/arch/x86/include/generated/uapi/asm/types.h"
-#endif
+#include <asm-generic/types.h>
